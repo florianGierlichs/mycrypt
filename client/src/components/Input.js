1edf389ac7bf4b6d2ecc64cc72aa70f1a205ce5a
florianGierlichs <gierlichs@gmx.de>
2020-04-09 12:59:40 +0200

    Add Input component

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -2,13 +2,13 @@ import styled from '@emotion/styled';
 import COLORS from '../utils/COLORS';
 
 const TextInput = styled.input`
-  border: 1px solid ${COLORS.primaryYellow};
   background-color: ${COLORS.primaryInputBackground};
   color: ${COLORS.primaryFont};
   font-size: 20px;
   border-radius: 14px;
-  width: 200px;
-  height: 50px;
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  padding-left: 15px;
 `;
 
 export default TextInput;
