d4f70da2adaa28f6d8dec76530318ecbfa00a047
florianGierlichs <gierlichs@gmx.de>
2020-04-10 12:26:11 +0200

    Optimize name components and brand design

--- client/src/components/Brand.js
+++ client/src/components/Brand.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Logo from '../assets/mycrypt-logo.svg';
 import styled from '@emotion/styled';
-import LogoImage from './LogoImage';
+import BrandImage from './BrandImage';
 import BrandTitle from './BrandTitle';
 
 const BrandContainer = styled.div`
   display: flex;
+  align-items: center;
 `;
 
 function Brand(props) {
   return (
     <BrandContainer>
-      <LogoImage src={Logo} alt="mycrypt Logo" {...props} />
+      <BrandImage src={Logo} alt="mycrypt Logo" {...props} />
       <BrandTitle {...props}>mycrypt</BrandTitle>
     </BrandContainer>
   );
