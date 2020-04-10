89220416dd63e6ab248afacc606d11431ebef789
florianGierlichs <gierlichs@gmx.de>
2020-04-10 11:41:27 +0200

    Change CSS props for better convention. Add helper function to calculate width and height of input component

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,13 +1,33 @@
 import styled from '@emotion/styled';
 import colors from '../utils/colorsUtils';
 
+const calculateWidth = (props) => {
+  if (props.Login) {
+    return '250px';
+  } else if (props.Search) {
+    return '180px';
+  } else {
+    return '140px';
+  }
+};
+
+const calculateHeight = (props) => {
+  if (props.Login) {
+    return '50px';
+  } else if (props.Search) {
+    return '30px';
+  } else {
+    return '30px';
+  }
+};
+
 const Input = styled.input`
   background-color: ${colors.primaryInputBackground};
   color: ${colors.primaryFont};
-  font-size: ${(props) => props.fontSize};
+  font-size: 20px;
   border-radius: 14px;
-  width: ${(props) => props.width};
-  height: ${(props) => props.height};
+  width: ${(props) => calculateWidth(props)};
+  height: ${(props) => calculateHeight(props)};
   padding-left: 15px;
   border: none;
   &:focus {
