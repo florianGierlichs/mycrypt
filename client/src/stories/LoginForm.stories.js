d0ca516f144e2297a17e25e7a1e67fb5dc968956
florianGierlichs <gierlichs@gmx.de>
2020-04-11 13:13:09 +0200

    Add Login form

---
+++ client/src/stories/LoginForm.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import LoginForm from '../components/LoginForm';
+
+export default {
+  title: 'Login',
+  component: LoginForm,
+};
+
+export const Login = () => <LoginForm />;
