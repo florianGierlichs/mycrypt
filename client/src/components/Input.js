4bc3626db1a6ffb36f57e0412eb44cd543c96b61
florianGierlichs <gierlichs@gmx.de>
2020-04-11 11:47:11 +0200

    Rework Input component, to have different size options

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,12 +1,25 @@
 import styled from '@emotion/styled';
 import colors from '../utils/colorsUtils';
 
+const inputSize = {
+  large: { padding: '10px', margin: '7px' },
+};
+
+const getInputPadding = (size) => {
+  return inputSize[size].padding;
+};
+
+const getInputMargin = (size) => {
+  return inputSize[size].margin;
+};
+
 const Input = styled.input`
   background-color: ${colors.primaryInputBackground};
   color: ${colors.primaryFont};
   font-size: 20px;
   border-radius: 14px;
-  padding-left: 15px;
+  padding: ${(props) => getInputPadding(props.size)};
+  margin: ${(props) => getInputMargin(props.size)};
   border: none;
   &:focus {
     outline: none;
