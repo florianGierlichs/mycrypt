18e25fe7a03cc797f35af43164e5aa575277d147
florianGierlichs <gierlichs@gmx.de>
2020-04-14 17:07:42 +0200

    Import GlobalStyles into App

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import useWindowDimensions from './utils/hooks';
 import Brand from './components/Brand';
 import ParticlesBackground from './components/ParticlesBackground';
+import GlobalStyles from './components/GlobalStyles';
 
 function App() {
   const { height, width } = useWindowDimensions();
   const particleNumbers = (height + width) / 20;
   return (
     <>
+      <GlobalStyles />
       <Brand size="big" />
       <ParticlesBackground {...particleNumbers} />
     </>
