12f881622c6650469ffdbad6f35b8ba4cdfe2a2a
florianGierlichs <gierlichs@gmx.de>
2020-04-14 18:07:19 +0200

    Outsource particle calculation

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import useWindowDimensions from './utils/hooks';
 import Brand from './components/Brand';
 import ParticlesBackground from './components/ParticlesBackground';
 import GlobalStyles from './components/GlobalStyles';
 
 function App() {
-  const { height, width } = useWindowDimensions();
-  const particleNumbers = (height + width) / 20;
   return (
     <>
       <GlobalStyles />
       <Brand size="big" />
-      <ParticlesBackground {...particleNumbers} />
+      <ParticlesBackground />
     </>
   );
 }
