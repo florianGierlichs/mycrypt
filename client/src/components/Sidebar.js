724570ed9b1b9f09cfaa3960a12ccb096d7f821f
florianGierlichs <gierlichs@gmx.de>
2020-04-16 17:29:55 +0200

    Add Sidbar component and storybook

---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import colors from '../utils/colors';
+import Brand from './Brand';
+import Menu from '../assets/dropdown-burger-menu.svg';
+
+const SidbarContainer = styled.div`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  background-color: ${colors.backgroundPrimary};
+  padding: 45px 15px 20px 15px;
+  display: flex;
+  justify-content: space-between;
+  border-radius: 0 0 24px 24px;
+  border: solid 1px ${colors.complementaryPrimary};
+`;
+
+const SidebarDropdownButton = styled.button`
+  background-color: ${colors.backgroundPrimary};
+  border: none;
+  width: 50px;
+  &:active {
+    transform: scale(0.9);
+  }
+`;
+
+const DropdownImage = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+export default function Sidebar() {
+  return (
+    <SidbarContainer>
+      <Brand size="small" />
+      <SidebarDropdownButton>
+        <DropdownImage src={Menu} alt="Burger-menu dropdown button" />
+      </SidebarDropdownButton>
+    </SidbarContainer>
+  );
+}
