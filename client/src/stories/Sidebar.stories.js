2f604b8fb6bf9895490b2d8be7a38e876cd2adfe
florianGierlichs <gierlichs@gmx.de>
2020-04-16 17:45:27 +0200

    Refactor Sidebar to Header

--- client/src/stories/Sidebar.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Sidebar from '../components/Sidebar';
-
-export default {
-  title: 'Sidebar',
-  component: Sidebar,
-};
-
-export const SidebarMobile = () => <Sidebar />;
