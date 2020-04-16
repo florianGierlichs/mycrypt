e89d3b3b60e95d88b90f8904d13e41f54adfcac6
florianGierlichs <gierlichs@gmx.de>
2020-04-16 17:29:55 +0200

    Add Sidbar component and storybook

---
+++ client/src/stories/Sidebar.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Sidebar from '../components/Sidebar';
+
+export default {
+  title: 'Sidebar',
+  component: Sidebar,
+};
+
+export const SidebarMobile = () => <Sidebar />;
