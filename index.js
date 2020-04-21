25f9b35561fcdfa998f913799f687c377c820e4c
florianGierlichs <gierlichs@gmx.de>
2020-04-21 17:47:41 +0200

    Import user route to server

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const port = process.env.PORT || 8080;
 
+//Import Routes
+app.use(express.json());
+
+const usersRoute = require('./lib/routes/users');
+
+app.use('/users', usersRoute);
+
+//
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('*', (request, response) => {
