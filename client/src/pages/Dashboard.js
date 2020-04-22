67032c7ea190285562f315d3e6aae01679468547
florianGierlichs <gierlichs@gmx.de>
2020-04-22 13:56:18 +0200

    Add dashboard page

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function Dashboard() {
+  return (
+    <>
+      <Header
+        userName="username123"
+        lastLogin="01.04.2020 - 19:01"
+        portfolio="8576,34"
+      />
+    </>
+  );
+}
+
+export default Dashboard;
