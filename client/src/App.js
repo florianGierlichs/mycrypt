cdfb4475fb5e8388acbf22c8ba8d5513b5285fac
florianGierlichs <gierlichs@gmx.de>
2020-04-22 12:03:00 +0200

    Add react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import Brand from './components/Brand';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ParticlesBackground from './components/ParticlesBackground';
 import GlobalStyles from './components/GlobalStyles';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 function App() {
   return (
     <>
-      <GlobalStyles />
-      <Brand size="big" />
-      <ParticlesBackground />
+      <Router>
+        <GlobalStyles />
+        <ParticlesBackground />
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route exact path="/Login">
+            <Login />
+          </Route>
+          <Route exact path="/Signup">
+            <Signup />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
