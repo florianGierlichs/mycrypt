24f824d19c87838eac91f27d6fefa468e242ea28
florianGierlichs <gierlichs@gmx.de>
2020-04-24 15:12:12 +0200

    Add react context for cards

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Header from '../components/Header';
+import { CardProvider } from '../utils/CardContext';
 
 function Dashboard() {
   return (
     <>
-      <Header
-        userName="username123"
-        lastLogin="01.04.2020 - 19:01"
-        portfolio="8576,34"
-      />
+      <CardProvider>
+        <Header
+          userName="username123"
+          lastLogin="01.04.2020 - 19:01"
+          portfolio="8576,34"
+        />
+      </CardProvider>
     </>
   );
 }
