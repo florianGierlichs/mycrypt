ca67746745abafdedadf869b46825763f6804802
florianGierlichs <gierlichs@gmx.de>
2020-04-29 14:41:57 +0200

    Rework Cards

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { CardContext } from '../utils/CardContext';
 import styled from '@emotion/styled';
 import Card from './Card';
@@ -9,32 +9,17 @@ const CardContainer = styled.div`
 `;
 
 export default function Cards() {
-  const [cards, setCards] = useContext(CardContext);
-  const username = 'uppercase';
-
-  console.log('!cards initial', cards);
-
-  async function getUserCoins() {
-    try {
-      const response = await fetch(`/api/users/${username}/coins/`);
-      const userCoins = await response.json();
-      console.log('!userCoins beim rendern', userCoins);
-      setCards(userCoins);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  getUserCoins();
-
-  useEffect(() => {
-    getUserCoins();
-  }, []);
+  const [cards] = useContext(CardContext);
 
   return (
     <CardContainer>
-      {cards?.map(({ name, price, symbol }) => (
-        <Card title={name} price={price} symbol={symbol} key={name} />
+      {cards?.map(({ name, priceUsd, symbol }) => (
+        <Card
+          title={name}
+          price={Number(priceUsd)}
+          symbol={symbol}
+          key={name}
+        />
       ))}
     </CardContainer>
   );
