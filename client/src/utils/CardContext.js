60fc4d6ed583bf790d71ede46134e7c81e9eb316
florianGierlichs <gierlichs@gmx.de>
2020-04-29 14:37:49 +0200

    Rework Card context

--- client/src/utils/CardContext.js
+++ client/src/utils/CardContext.js
@@ -1,12 +1,51 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { UsernameContext } from './UsernameContext';
+import { CoinDataContext } from './CoinDataContext';
 
 export const CardContext = createContext();
 
 export const CardProvider = ({ children }) => {
   const [cards, setCards] = useState([]);
+  const [coinData] = useContext(CoinDataContext);
+  const [username] = useContext(UsernameContext);
+
+  function filterCoins(userCoins = []) {
+    let filteredCoins = [];
+    userCoins.forEach((coin) => {
+      coinData.forEach((item) => {
+        if (item.name === coin.name) {
+          filteredCoins = [...filteredCoins, { ...coin, ...item }];
+        }
+      });
+    });
+    return filteredCoins;
+  }
+
+  async function getUserCoins() {
+    try {
+      const response = await fetch(`/api/users/${username}/coins/`);
+      const userCoins = await response.json();
+
+      return userCoins;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function getUserCardData() {
+    const userCoins = await getUserCoins();
+    const filteredCoins = filterCoins(userCoins);
+
+    setCards(filteredCoins);
+  }
+
+  useEffect(() => {
+    getUserCardData();
+  }, [coinData]);
+
   return (
-    <CardContext.Provider value={[cards, setCards]}>
+    <CardContext.Provider value={[cards, getUserCardData]}>
       {children}
     </CardContext.Provider>
   );
