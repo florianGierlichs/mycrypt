f8df657075823d52a6aec3b33a751d13cd7998c5
florianGierlichs <gierlichs@gmx.de>
2020-04-29 18:01:24 +0200

    Restructure

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const usersRoute = require('./lib/routes/users');
+const coinsRoute = require('./lib/routes/coins');
+
 const app = express();
 const port = process.env.PORT || 8080;
-const mongoose = require('mongoose');
+
+//Import Routes
+app.use(express.json());
+
+app.use('/api/users', usersRoute);
+app.use('/api/coins', coinsRoute);
+
+//
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
 
 //Connect to DB
 mongoose.connect(
@@ -14,24 +30,11 @@ mongoose.connect(
     dbName: process.env.DB_NAME,
     useFindAndModify: false,
   },
+  () => {
+    console.log('connected to db!');
 
-  () => console.log('connected to db!')
+    app.listen(port, () => {
+      console.log(`server is running http://localhost:${port}`);
+    });
+  }
 );
-
-//Import Routes
-app.use(express.json());
-
-const usersRoute = require('./lib/routes/users');
-
-app.use('/api/users', usersRoute);
-
-//
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-app.get('*', (request, response) => {
-  response.sendFile(path.join(__dirname, 'client/build/index.html'));
-});
-
-app.listen(port, () => {
-  console.log(`server is running http://localhost:${port}`);
-});
