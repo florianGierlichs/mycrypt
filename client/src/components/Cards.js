af8726c65e533ea2da6f3f2e01a71795a632aa3b
florianGierlichs <gierlichs@gmx.de>
2020-05-01 12:32:24 +0200

    Add card carousel-slider

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -2,25 +2,30 @@ import React, { useContext } from 'react';
 import { CardContext } from '../utils/CardContext';
 import styled from '@emotion/styled';
 import Card from './Card';
+import SliderCustom from './SliderCustom';
+
+const CardsContainer = styled.div`
+  padding-top: 150px;
+  display: flex;
+`;
 
 const CardContainer = styled.div`
-  padding-top: 200px;
   display: flex;
+  justify-content: center;
 `;
 
 export default function Cards() {
   const [cards] = useContext(CardContext);
 
   return (
-    <CardContainer>
-      {cards?.map(({ name, priceUsd, symbol }) => (
-        <Card
-          title={name}
-          price={Number(priceUsd)}
-          symbol={symbol}
-          key={name}
-        />
-      ))}
-    </CardContainer>
+    <CardsContainer>
+      <SliderCustom>
+        {cards?.map(({ name, priceUsd, symbol }) => (
+          <CardContainer key={name}>
+            <Card title={name} price={Number(priceUsd)} symbol={symbol} />
+          </CardContainer>
+        ))}
+      </SliderCustom>
+    </CardsContainer>
   );
 }
