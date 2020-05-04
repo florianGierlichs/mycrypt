21a57bc6894562001c61d83f4b30e933fd46039e
florianWindows <florian@DESKTOP-4C1J2NP.localdomain>
2020-05-04 12:24:25 +0200

    Refactor fetch. Fix response error

--- client/src/components/SearchList.js
+++ client/src/components/SearchList.js
@@ -64,7 +64,7 @@ export default function SearchList({ searchResults, username }) {
           }),
         });
 
-        if (response.status !== 200) {
+        if (!response.ok) {
           throw new Error(response.error);
         }
       } catch (error) {
