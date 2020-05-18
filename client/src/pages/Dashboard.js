499ec9549702feae20811ba61e137ae00e65d435
florianGierlichs <gierlichs@gmx.de>
2020-05-18 11:41:22 +0200

    Refactor responsive layout

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -3,10 +3,13 @@ import Header from '../components/Header';
 import { CoinProvider } from '../utils/CoinContext';
 import Cards from '../components/Cards';
 import { useHistory } from 'react-router-dom';
+import useWindowDimensions from '../utils/hooks';
+import Sidebar from '../components/Sidebar';
 
 function Dashboard() {
   const username = localStorage.getItem('username');
   const history = useHistory();
+  const { width } = useWindowDimensions();
 
   if (!username) {
     history.push(`/login`);
@@ -15,7 +18,16 @@ function Dashboard() {
   return (
     <>
       <CoinProvider>
-        <Header username={username} lastLogin="08.05.2020 - 19:01" />
+        {width < 1024 ? (
+          <Header username={username} lastLogin="08.05.2020 - 19:01" />
+        ) : (
+          <Sidebar
+            active={true}
+            username={username}
+            lastLogin="08.05.2020 - 19:01"
+          />
+        )}
+
         <Cards />
       </CoinProvider>
     </>
